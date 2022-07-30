01e0438eecd7a4a165f69fe319eab7410e46c916
sumitk-hub1999 <skatyal1999@gmail.com>
2022-07-30 08:18:58 +0530

    redux app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import React from 'react';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Project Setup
+    </div>
+  );
+}
+
+export default App;
