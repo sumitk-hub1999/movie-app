266fa08fcb8270595f80bae30e371f4fb05705e8
sumitk-hub1999 <skatyal1999@gmail.com>
2022-08-01 08:32:58 +0530

    moviecard

---
+++ src/components/MovieCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class MovieCard extends React.Component{
+
+    
+    render(){
+        const {movie}=this.props;
+        return(
+            <div className="movie-card">
+               <div className="left">
+                   <img alt="movie-poster" src={movie.Poster} />
+               </div>
+               <div className="right">
+                  <div className="title">
+                     {movie.title}
+                  </div>
+                  <div className="plot">
+                     {movie.Plot}
+                  </div>
+                  <div className="footer">
+                    <div className="rating">{movie.imdbRating}</div>
+                    <button className="favourite-btn">Favourite</button>
+                  </div>
+                   
+               </div>
+            </div>
+        );
+    }
+}
+
+export default MovieCard;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import React from 'react';
+//import './App.css';
+class Navbar extends React.Component{
+    render() {
+        return (
+          <div className="nav">
+            <div className='search-container'>
+              <input />
+              <button id="search-btn">search</button>
+            </div>
+          </div>
+        );}
+}
+
+
+
+export default Navbar ;
