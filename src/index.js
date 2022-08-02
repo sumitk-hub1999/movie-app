31430bbc1e27e7709110f91bfc7150209d6b9cfe
sumitk-hub1999 <skatyal1999@gmail.com>
2022-08-02 16:22:00 +0530

    dispatching actions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
-//import reportWebVitals from './reportWebVitals';
 
+import movies from './reducers';
+//import reportWebVitals from './reportWebVitals';
+const store=createStore(movies);
+
+console.log(store);
+console.log(store.getState());
+
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'Superman'}]
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
